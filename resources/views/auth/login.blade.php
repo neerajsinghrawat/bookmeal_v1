7ecb243e4e0438d255269e64d79b5bdfd3475c99
vishal <vishalsinghbanna2013@gmail.com>
2020-08-29 15:25:45 +0530

    test@gmail.com

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,6 +3,57 @@
 @section('description','Login')
 @section('keywords', 'Login')
 @section('content')
+        <section class="section section-lg bg-dark">
+
+            <!-- Video BG -->
+
+            <!-- BG Video -->
+            <div class="bg-video dark-overlay" data-src="http://assets.suelo.pl/soup/video/video_3.mp4" data-type="video/mp4"></div>
+
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-6">
+                        <!-- Book a Table -->
+                        <div class="utility-box">
+                            <div class="utility-box-title bg-dark dark">
+                                <div class="bg-image"><img src="http://assets.suelo.pl/soup/img/photos/modal-review.jpg" alt=""></div>
+                                <div>
+                                    <span class="icon icon-primary"><i class="ti ti-bookmark-alt"></i></span>
+                                    <h4 class="mb-0">Login</h4>
+                                    <p class="lead text-muted mb-0">Don't have an account? So <a href="{{ route('register') }}">Register</a> And starts less than a minute</p></p>
+                                </div>
+                            </div>
+                           
+                            <form method="post" enctype="multipart/form-data" action="{{ route('login') }}">
+                            {{ csrf_field() }}
+                                <div class="utility-box-content">
+                                            <div class="form-group">
+                                                <i class="icofont icofont-ui-message"></i><input type="email" name="email" value="" placeholder="EMAIL" id="email" class="form-control" required/>
+                                            @if ($errors->has('email'))
+                                                <span class="help-block">
+                                                    <strong>{{ $errors->first('email') }}</strong>
+                                                </span>
+                                            @endif
+                                            </div>
+
+                                            <div class="form-group">
+                                                <i class="icofont icofont-lock"></i><input type="password" name="password" value="" placeholder="PASSWORD" id="input-password" class="form-control" required/>
+                                            @if ($errors->has('password'))
+                                                <span class="help-block">
+                                                    <strong>{{ $errors->first('password') }}</strong>
+                                                </span>
+                                            @endif
+                                            </div>
+                                </div>
+                                <button class="utility-box-btn btn btn-secondary btn-block btn-lg" type="submit">SIGN IN</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
 
             <!-- Breadcrumb Start -->
             <div class="bread-crumb">
